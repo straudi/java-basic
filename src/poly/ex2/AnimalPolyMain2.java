928c08ce8b4511a7f958c8cd9db06c6d64bd0f7c
황승아 <mina789@naver.com>
2024-07-31 00:43:51 +0900

    다형성

--- src/poly/ex2/AnimalPolyMain2.java
+++ src/poly/ex2/AnimalPolyMain2.java
@@ -2,20 +2,25 @@ package poly.ex2;
 
 public class AnimalPolyMain2 {
     public static void main(String[] args) {
-        Dog dog = new Dog();
-        Cat cat = new Cat();
-        Caw caw = new Caw();
-        Duck duck = new Duck();
+        //Dog dog = new Dog();
+        //Cat cat = new Cat();
+        //Caw caw = new Caw();
+        //Duck duck = new Duck();
+        Animal[] animalArr = {new Dog(), new Cat(), new Caw(), new Duck()};
+        
+        // 변하지 않는 부분
+        for (Animal animal : animalArr) {
+            soundAnimal(animal);
+        }
 
-        SoundAnimal(dog);
-        SoundAnimal(cat);
-        SoundAnimal(caw);
-        SoundAnimal(duck);
     }
 
-    private static void SoundAnimal(Animal animal){
+    // 변하지 않는 부분
+    private static void soundAnimal(Animal animal) {
         System.out.println("동물 소리 테스트 시작");
         animal.sound();
         System.out.println("동물 소리 테스트 종료");
     }
+
+
 }
---
+++ src/poly/ex4/AbstractAnimal.java
@@ -0,0 +1,12 @@
+package poly.ex4;
+
+public abstract class AbstractAnimal {
+    public abstract void sound();
+
+    public abstract void move();
+
+    public void Hello(){
+        System.out.println("인사");
+    }
+
+}
